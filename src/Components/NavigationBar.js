412323d8ff4ec1130b0da68ca632fa976cdce250
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-04-22 18:47:52 +0530

    Created a job results page

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -9,7 +9,10 @@ const NavigationBar = () => {
             <header className='header'>
             <Link to="/" className="nav-title"><h1>Web Scraper</h1></Link>
             <div className='topnav-right'>
-                <Link to="/" className="nav-link"><h2>Search Jobs</h2></Link>
+                <Link to="/" className="nav-link">
+                    <h2>Search Jobs</h2>
+                </Link>
+                <Link to='/job-results' className='nav-link'><h2>Job Results</h2></Link>
                 <Link to="/login" className="nav-link"><h2>Log In</h2></Link>
                 <Link to="/signup" className="nav-link"><h2>Create Account</h2></Link>
             </div>
