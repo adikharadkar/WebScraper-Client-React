674b69b1dde8d8a229667cec1672ad67e663678c
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-05-23 01:16:49 +0530

    Update NavigationBar.js

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -10,9 +10,7 @@ const NavigationBar = () => {
             <header className='header'>
             <Link to="/" className="nav-title"><h1>Web Scraper</h1></Link>
             <div className='topnav-right'>
-                <Link to="/" className="nav-link">
-                    <h2>Search Jobs</h2>
-                </Link>
+                
                 <Link to="/login" className="nav-link"><h2>Log In</h2></Link>
                 <Link to="/signup" className="nav-link"><h2>Create Account</h2></Link>
             </div>
